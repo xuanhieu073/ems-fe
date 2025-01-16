a9f52902d546a9041ad2381b2f30e7d682da3a19
Nguyen Xuan Hieu <32063031+xuanhieu073@users.noreply.github.com>
2025-01-16 15:26:47 +0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -22,8 +22,8 @@ def deployApp() {
     // def dockerCmd = "docker run -p 8080:8080 -d xuanhieu073/demo-app:jma-2.0"
     def dockerComposeCmd = "docker-compose -f docker-compose.yaml up --detach"
     sshagent(['my-server-key']) {
-        sh "scp -v -o StrictHostKeyChecking=no docker-compose.yaml ec2-user@ec2-54-169-193-218.ap-southeast-1.compute.amazonaws.com:/home/ec2-user"
-        sh "ssh -o StrictHostKeyChecking=no ec2-user@ec2-54-169-193-218.ap-southeast-1.compute.amazonaws.com ${dockerComposeCmd}"
+        sh "ssh -o StrictHostKeyChecking=no ec2-user@ec2-54-179-151-144.ap-southeast-1.compute.amazonaws.com ${dockerComposeCmd}"
+        sh "scp -v -o StrictHostKeyChecking=no docker-compose.yaml ec2-user@ec2-54-179-151-144.ap-southeast-1.compute.amazonaws.com:/home/ec2-user"
     }
 }
 
